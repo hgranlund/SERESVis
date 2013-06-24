f020e4438491ce85066cea001699f29b59f0a43d
Simen Granlund <simen@hgranlund.com>
2013-06-24 10:31:23 +0200

    Make a static node server

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
 var http = require('http');
-var url = require('url');
-var fs = require('fs');
+var express = require('express');
+var ecstatic = require('ecstatic');
 
-var newPostFormHTML = fs.readFileSync('index.html');
+var app = express();
+app.use(ecstatic({ root: __dirname + '/public' }));
+http.createServer(app).listen(8080);
 
-function renderNewPostForm(request, response) {
-    response.writeHead(200, {
-        'Content-type': 'text/html; charset=utf-8'
-    });
-    response.end(newPostFormHTML);
-}
-
-function render404(request, response) {
-    response.writeHead(404);
-    response.end('404 File not found');
-}
-
-var server = http.createServer(function(request, response) {
-    var newPostFormRegex = new RegExp('^/?$');
-    var pathname = url.parse(request.url).pathname;
-    if (newPostFormRegex.test(pathname)) {
-  renderNewPostForm(request, response);
-    } else {
-  render404(request, response);
-    } 
-});
-
-server.listen(8000);
-
-console.log('Listening on http://127.0.0.1:8000');
+console.log('Listening on :8080');
