cfa9aaaaf25943c205e2b19836f76a5a5c147bae
Simen Granlund <simen@hgranlund.com>
2013-06-25 15:01:33 +0200

    clead repo

--- test/browser-test.js
+++
@@ -1,9 +0,0 @@
-// Browser tests
-var buster = require("buster");
-var myLib = require("../lib/my-lib");
-
-buster.testCase("A module", {
-    "states the obvious": function () {
-        assert(true);
-    }
-});
