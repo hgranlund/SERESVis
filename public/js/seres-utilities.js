958b991b324397dda3ada743927ba360c10cbc9e
Simen Granlund <simen@hgranlund.com>
2013-07-22 16:20:07 +0200

    Modified color functions

--- public/js/seres-utilities.js
+++ public/js/seres-utilities.js
@@ -2,12 +2,48 @@ window.seres.utilities = function (d3) {
 
     var color = [d3.rgb('#1f77b4'), d3.rgb('#aec7e8'), d3.rgb('#ff7f0e'), d3.rgb('#ffbb78'), d3.rgb('#2ca02c'), d3.rgb('#98df8a'), d3.rgb('#d62728'), d3.rgb('#ff9896'), d3.rgb('#9467bd'), d3.rgb('#c5b0d5'), d3.rgb('#8c564b'), d3.rgb('#c49c94'), d3.rgb('#e377c2'), d3.rgb('#f7b6d2'), d3.rgb('#7f7f7f'), d3.rgb('#c7c7c7'), d3.rgb('#bcbd22'), d3.rgb('#dbdb8d'), d3.rgb('#17becf'), d3.rgb('#9edae5')];
 
-    var getColor = function (d) {
-        var children = d.children || d.individuals || [];
+    var _getColor = function (d) {
+        var children = d.children || d.isIndividual || [];
         var colorNum = d.name.split('').length + children.length;
         colorNum = colorNum * 123;
         colorNum = colorNum % 21;
         return color[colorNum];
+    };
+
+    var getColor = function (d, parent) {
+        if (d.isIndividual || d.isProperty) {
+            if (parent) {
+                return _getColor(parent);
+            } else {
+                return _getColor(d);
+            }
+            return _getColor(d)
+        } else {
+            return _getColor(d);
+        }
+    };
+
+    var getStroke = function (d, parent) {
+        return _getColor(parent);
+    };
+
+    var getParentColor = function (d, formatter) {
+        var parentId = getPropertyValue('subClassOf', d.object);
+        if (parentId) {
+            var parentNode = formatter.createNode(parentId, 0);
+            return _getColor(parentNode);
+        } else {
+            return getColor(d);
+        }
+    };
+
+    var getParent = function (d, formatter) {
+        var parentId = getPropertyValue('subClassOf', d.object) || getPropertyValue('type', d.object);
+        if (parentId !== 'Class') {
+            return formatter.createNode(parentId, 0);
+        } else {
+            return null;
+        }
     };
 
     var toLegalHtmlName = function (className) {
@@ -63,25 +99,37 @@ window.seres.utilities = function (d3) {
         return false;
     };
 
-    var getParentColor = function (d, formatter) {
-        var parentId = getPropertyValue('subClassOf', d.object);
-        if (parentId) {
-            var parentNode = formatter.createNode(parentId, 0);
-            return getColor(parentNode);
-        } else {
-            return getColor(d);
+
+    var isEmpty = function (obj) {
+
+        if (obj == null) {
+            return true;
         }
-    };
+        if (obj.length && obj.length > 0) {
+            return false;
+        }
+        if (obj.length === 0) {
+            return true;
+        }
+        for (var key in obj) {
+            if (hasOwnProperty.call(obj, key)) return false;
+        }
+        return true;
+    }
 
 
     return {
         getColor: getColor,
+        getStroke: getStroke,
         toLegalHtmlName: toLegalHtmlName,
         getPropertyValue: getPropertyValue,
         addNodeToNodes: addNodeToNodes,
         getNode: getNode,
         getParentColor: getParentColor,
-        getLinkWithNodeId: getLinkWithNodeId
+        getLinkWithNodeId: getLinkWithNodeId,
+        isEmpty: isEmpty,
+        getParent: getParent
+
     };
 
 
